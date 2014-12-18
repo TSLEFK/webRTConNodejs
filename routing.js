947d5af7932bfc0cc07fd4256ba89b172511ca89
TSLEFK <satarn.sherlock@gmail.com>
2014-12-18 18:46:29 +0900

    create test.html

--- routing.js
+++ routing.js
@@ -18,23 +18,8 @@ module.exports = function(app){
   app.get('/test', function(req, res){ 
     res.render('test', {test: 'TEST page XD'});
   });
-  
-   /* 
-  app.get('/index.js',function(req, res){
-    res.sendfile(__dirname+ '/index.js');
+  app.get('/test.html',function(req, res){ 
+    res.sendfile(__dirname+ '/test.html');
   });
-  
-  app.get('/webrtc.io.js',function(req, res){
-    res.sendfile(__dirname+ '/webrtc.io.js');
-  });
-
-  app.get('/jquery.js', function(req, res){
-    res.sendfile(__dirname + '/jquery-1.11.1.js');
-  });
-  
-  app.get('/script.js', function(req, res){
-    res.sendfile(__dirname + '/script.js');
-  });
-  */
 
 }
--- server.js
+++ server.js
@@ -18,4 +18,87 @@ server.listen(port);
 
 console.log("Server is listening on port " + port);
 
+/*
+ *
+//var user_numbers = 1000;
+var userList={}; // {num : socket }
 
+// data = {num => user_id }
+webRTC.rtc.on('call',function(data,socket){
+  var soc;
+  for(key in userList){
+    if(key == data.num){ //相手のIDが存在するなら
+      soc = userList[key];
+      var tmp = [socket.id];
+      webRTC.rtc.rooms[data.my_id] = tmp;
+    }else{
+    }
+  }
+  console.log(webRTC.rtc.rooms);
+
+  if (soc){
+    console.log(data.num + "'s socketID is " + soc.id);
+    soc.send(JSON.stringify({
+      "eventName" : "receive_tell",
+      "data" : {"str" : "電話がきました",
+                "room" : data.my_id
+      }
+    }));
+  }else{
+    console.log(data.num + "is not found...")
+  }
+});
+
+webRTC.rtc.on('create_userId',function(data,socket){
+  var num = 1001;
+  /* cookieで使用されいている番号に入らないように *
+  for(key in userList){
+    console.log(key);
+    if(key == num)
+      num +=1;
+  }
+  console.log("num : " + num);
+  console.log("socketid : " + socket.id);
+  userList[num] = socket;
+
+  socket.send(JSON.stringify({
+    "eventName" : "receive_user_id",
+    "data" : {
+      "userId" : num ,
+      "socketId" : socket.id
+    }
+  }));
+});
+
+webRTC.rtc.on("update_userId",function(data,socket){
+  userList[data.u_id] = socket;
+  console.log(data.u_id);
+  console.log("socketid:" + userList[data.u_id].id);
+  socket.send(JSON.stringify({
+    "eventName" : "receive_user_id",
+    "data" : {
+      "userId" : data.u_id ,
+      "socketId" : socket.id
+    }
+  }));
+});
+
+webRTC.rtc.on('close',function(data,socket){
+  /* rootpathにリダイレクトすることで切れる*
+  var roomList = webRTC.rtc.rooms[data.room] || [];
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+    var soc = webRTC.rtc.getSocket(socketId);//socketが取得できる
+
+    if (soc) {
+      soc.send(JSON.stringify({
+        "eventName": "disconnect_tell"
+      }), function(error) {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
+  }
+});
+*/
