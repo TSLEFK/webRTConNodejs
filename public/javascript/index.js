b7d66b0fcb7c1ad9380c9643321fa951a5b345ca
TSLEFK <satarn.sherlock@gmail.com>
2014-12-23 21:22:14 +0900

    complete cookie&connectiong

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,18 +1,28 @@
+/*ダイアルの操作*/
 function dial(push_dial){
         document.getElementById("number").value += push_dial;
 }
-
-function delCookie(){
-        cName = "video_chat"; //削除するクッキー名
-        $.cookie( cName , ' ', { expires: -1} );
-}
-
 
 $( function() {
         $("#clear").click( function () {
 	$("#number").val("");
         } ) ;
         
+         $("#delete").click(function(){
+	var tmp = $("#number").val();
+	var tmp2 = tmp.slice(0, -1);
+	$("#number").val( tmp2 );
+        });  
+});
+
+//クッキーの削除
+function delCookie(){
+        cName = "video_chat"; //削除するクッキー名
+        $.cookie( cName , ' ', { expires: -1} );
+}
+
+//自分のカメラも表示させるか
+$( function() {
         $("#switch_me").click( function(){
 	$("#me").toggle();
 	
@@ -21,12 +31,5 @@ $( function() {
 	}else{
 	        $("#switch_me").text("自分のカメラOFF");
 	}
-        });
-        
-        $("#delete").click(function(){
-	var tmp = $("#number").val();
-	var tmp2 = tmp.slice(0, -1);
-	$("#number").val( tmp2 );
-        });
-        
+        });   
 });
--- routing.js
+++ routing.js
@@ -18,8 +18,12 @@ module.exports = function(app){
   app.get('/test', function(req, res){ 
     res.render('test', {test: 'TEST page XD'});
   });
+  
   app.get('/test.html',function(req, res){ 
     res.sendfile(__dirname+ '/test.html');
   });
-
+  
+    app.get('/memo', function (req, res){
+    res.render('memo', {title: 'ExpTEST Hello!!'});
+  });
 }
